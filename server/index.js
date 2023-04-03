8432824394861f35825c11543936edb839ca6f85
micposso <micposso@gmail.com>
2023-04-03 11:19:23 -0400

    fixed server and client

--- server/index.js
+++ server/index.js
@@ -1,6 +1,21 @@
+import * as dotenv from 'dotenv'
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-const express = require('express');
+dotenv.config()
+const DB = process.env.DB;
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = DB;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+  .then(()=>{console.log(`Server running on ${PORT}`)})
+  .catch((error) => console.log(error.message));
+
